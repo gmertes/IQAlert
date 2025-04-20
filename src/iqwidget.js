2e7c02946dbfd938887254abc49382df52ba2669
Gert Mertes <hey@gertmertes.be>
2025-04-20 10:24:55 +0100

    Clean up widget

--- src/iqwidget.js
+++ src/iqwidget.js
@@ -11,97 +11,101 @@ class IQWidget {
         this.idClan = 'iqwidget_clanmobs';
         this.divMain = undefined;
         this.divClan = undefined;
+        this.attached = false;
     }
 
     attach() {
-        if (!document.getElementById(this.idMain)) {
-            this.divMain = document.createElement('div');
-            this.divMain.id = this.idMain;
-            this.divMain.classList.add('margin-top-small');
-            this.divMain.style.padding = '0.5rem';
-
-            this.divClan = document.createElement('div');
-            this.divClan.id = this.idClan;
-
-            this.divMain.appendChild(this.divClan);
-
-            try {
-                document.querySelector('.main-game-section').appendChild(this.divMain);
-            } catch { }
-
-            // check attachment
-            if (document.getElementById(this.idMain)) {
-                console.debug('Widget attached');
-                return true;
-            } else {
-                console.warn('Something went wrong with widget attachment');
-                return false;
-            }
-        } else {
+        if (this.attached)
             return true;
+
+        this.divMain = document.createElement('div');
+        this.divMain.id = this.idMain;
+        this.divMain.classList.add('margin-top-small');
+        this.divMain.style.padding = '0.5rem';
+
+        this.divClan = document.createElement('div');
+        this.divClan.id = this.idClan;
+
+        this.divMain.appendChild(this.divClan);
+
+        try {
+            document.querySelector('.main-game-section').appendChild(this.divMain);
+        } catch { }
+
+        if (!document.getElementById(this.idMain)) {
+            console.warn('Something went wrong with widget attachment');
+            return false;
         }
+
+        console.debug('Widget attached');
+        this.attached = true;
+        return true;
     }
 
     renderBattlegrounds(data) {
-        if (data && this.attach()) {
-            this.divClan.innerHTML = '';
+        if (!data)
+            return;
 
-            const clanTable = document.createElement('table');
-            clanTable.classList.add('table-invisible');
-            clanTable.innerHTML = '<caption class="heading">Clan Battlegrounds</caption>';
+        if (!this.attach())
+            return;
 
-            const tableHeader = document.createElement('tr');
-            tableHeader.innerHTML = '<th>Mob</th><th>Power Level</th><th>Remaining</th>';
+        this.divClan.innerHTML = '';
 
-            clanTable.appendChild(tableHeader);
+        const clanTable = document.createElement('table');
+        clanTable.classList.add('table-invisible');
+        clanTable.innerHTML = '<caption class="heading">Clan Battlegrounds</caption>';
 
-            const mobs = data['mobData'];
-            const boss = data['bossData'];
-            // const mobs = [
-            //     {
-            //         "id": 1,
-            //         "name": "Clan Scavenger",
-            //         "powerLevel": 10,
-            //         "amount": 48
-            //     },
-            //     {
-            //         "id": 2,
-            //         "name": "Clan Invader",
-            //         "powerLevel": 100,
-            //         "amount": 18
-            //     }
-            // ]
-            // const boss = {
-            //     "id": 1,
-            //     "name": "Baby Dragon",
-            //     "rarity": 2,
-            //     "level": 2,
-            //     "hpMax": 200000,
-            //     "hpRemaining": 28354
-            // }
+        const tableHeader = document.createElement('tr');
+        tableHeader.innerHTML = '<th>Mob</th><th>Power Level</th><th>Remaining</th>';
 
-            if (boss) {
+        clanTable.appendChild(tableHeader);
+
+        const mobs = data['mobData'];
+        const boss = data['bossData'];
+        // const mobs = [
+        //     {
+        //         "id": 1,
+        //         "name": "Clan Scavenger",
+        //         "powerLevel": 10,
+        //         "amount": 48
+        //     },
+        //     {
+        //         "id": 2,
+        //         "name": "Clan Invader",
+        //         "powerLevel": 100,
+        //         "amount": 18
+        //     }
+        // ]
+        // const boss = {
+        //     "id": 1,
+        //     "name": "Baby Dragon",
+        //     "rarity": 2,
+        //     "level": 2,
+        //     "hpMax": 200000,
+        //     "hpRemaining": 28354
+        // }
+
+        if (boss) {
+            const tr = document.createElement('tr');
+            try {
+                tr.className = 'text-rarity-' + boss.rarity;
+            } catch {
+                tr.className = 'green-text';
+            }
+            tr.innerHTML = `<td>${boss.name}</td><td>over 9000!</td><td>${boss.hpRemaining.toLocaleString('en-US')} hp</td>`;
+            clanTable.appendChild(tr);
+        }
+
+        if (mobs.length > 0) {
+            mobs.forEach(mob => {
                 const tr = document.createElement('tr');
-                try {
-                    tr.className = 'text-rarity-' + boss.rarity;
-                } catch {
-                    tr.className = 'green-text';
-                }
-                tr.innerHTML = `<td>${boss.name}</td><td>over 9000!</td><td>${boss.hpRemaining.toLocaleString('en-US')} hp</td>`;
+                tr.innerHTML = `<td>${mob.name}</td><td>${mob.powerLevel.toLocaleString('en-US')}</td><td>${mob.amount}</td>`;
                 clanTable.appendChild(tr);
-            }
+            });
+        }
 
-            if (mobs.length > 0) {
-                mobs.forEach(mob => {
-                    const tr = document.createElement('tr');
-                    tr.innerHTML = `<td>${mob.name}</td><td>${mob.powerLevel.toLocaleString('en-US')}</td><td>${mob.amount}</td>`;
-                    clanTable.appendChild(tr);
-                });
-            }
-
-            if (mobs.length > 0 || boss) {
-                this.divClan.appendChild(clanTable);
-            }
+        if (mobs.length > 0 || boss) {
+            this.divClan.appendChild(clanTable);
         }
     }
 
